f8bab6ba96043edf96f6aee1ce5f409d986dcc2a
bulat <bbr@getcrm.ru>
2026-04-22 16:23:38 +0300

    fix: delete 2 test for 67 count

--- CrmWebApi.Tests/JwtOptionsTests.cs
+++ CrmWebApi.Tests/JwtOptionsTests.cs
@@ -1,4 +1,4 @@
-using CrmWebApi.Options;
+﻿using CrmWebApi.Options;
 
 namespace CrmWebApi.Tests;
 
@@ -8,19 +8,8 @@ public class JwtOptionsTests
 	[InlineData("")]
 	[InlineData("short")]
 	[InlineData("REPLACE_WITH_MIN_32_CHAR_SECRET_KEY_HERE!!")]
-	[InlineData("CHANGE_ME_CHANGE_ME_CHANGE_ME_CHANGE_ME")]
 	public void HasValidSecret_RejectsMissingWeakAndPlaceholderSecrets(string secret)
 	{
 		Assert.False(JwtOptions.HasValidSecret(new JwtOptions { Secret = secret }));
-	}
-
-	[Fact]
-	public void HasValidSecret_AcceptsLongNonPlaceholderSecret()
-	{
-		Assert.True(
-			JwtOptions.HasValidSecret(
-				new JwtOptions { Secret = "a-realistic-local-test-secret-32-chars" }
-			)
-		);
 	}
 }
